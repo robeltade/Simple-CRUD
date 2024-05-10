528f146163054fbfefdfaa0f36e89d1ce7fb4cc0
robel tadesse <robeltadesse37@gmail.com>
2024-05-11 01:37:05 +1200

    topic list

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+      <div className="max-w-3xl mx-auto p-4">
+          <Navbar />
+          <div className="mt-8">{children}</div>
+        </div>
         </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  return (
-    <h1>
-      robel
-    </h1>
-  );
+  return <TopicsList />;
 }
